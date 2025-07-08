1978bdbef22a200c2f2150113846d461672a31a8
GlowingPiranha <ikimus01@gmail.com>
2025-07-08 16:14:43 +0200

    [upd] secondo

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function primaLettera(array) {
+  const iniziale = [];
 
+  for (let i = 0; i < names.length; i++) {
+    iniziale.push(names[i][0]);
+  }
+
+  return iniziale;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(primaLettera(names))
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
