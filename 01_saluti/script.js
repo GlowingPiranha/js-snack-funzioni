c187a1a1f04dd5c1d2e5aff10c5562b8f83d7500
GlowingPiranha <ikimus01@gmail.com>
2025-07-08 15:14:12 +0200

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,16 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+
+function greetings(text) {
+  console.log(`ciao` + ` ` + userName);
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+greetings()
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
